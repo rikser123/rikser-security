bda595820a44519715bc1c9b53721abedf86dba7
Leonid Shikhalkin <rikser123@yandex.ru>
2025-10-29 17:36:36 +0300

    use bundle

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,16 +57,6 @@ repositories {
 val mockitoAgent = configurations.create("mockitoAgent")
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.5.6")
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
-	implementation("org.liquibase:liquibase-core")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
-	implementation("io.jsonwebtoken:jjwt:0.13.0")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
@@ -76,7 +66,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	mockitoAgent("org.mockito:mockito-core") { isTransitive = false }
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	implementation("rikser123:bundle:0.0.5")
+	implementation("rikser123:bundle:0.0.6")
 }
 
 tasks.withType<Test> {
